72445b39d5446125b83cd0340d8e77c9dd3698f3
mahedi <mahedianik4@gmail.com>
2023-06-02 19:20:47 +0600

    prettier check

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 // getting-started.js
-import mongoose  from 'mongoose';
-import app from './app';
-require('dotenv').config()
+import mongoose from 'mongoose'
+import app from './app'
+import dotenv from 'dotenv'
+dotenv.config()
 
 async function main() {
-try {
-    await mongoose.connect(process.env.DB_CONNECTION as string);
-    console.log("db connect successfully")
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION as string)
+    console.log('db connect successfully')
     app.listen(process.env.PORT, () => {
-        console.log(`university app listening on port ${process.env.PORT}`)
-      })
-} catch (error) {
-    console.log("connect felid",error)
-}
+      console.log(`university app listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.log('connect felid', error)
+  }
 }
 main()
