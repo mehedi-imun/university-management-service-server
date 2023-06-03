a437ca878939b135442fbb1421d4e1a38cfd3806
mahedi <mahedianik4@gmail.com>
2023-06-03 14:51:05 +0600

    add winston loger

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,18 @@
 import mongoose from 'mongoose'
 import app from './app'
 import dotenv from 'dotenv'
+import logger from './shared/loger'
 dotenv.config()
 
 async function main() {
   try {
     await mongoose.connect(process.env.DB_CONNECTION as string)
-    console.log('db connect successfully')
+    logger.info('db connect successfully')
     app.listen(process.env.PORT, () => {
-      console.log(`university app listening on port ${process.env.PORT}`)
+      logger.info(`university app listening on port ${process.env.PORT}`)
     })
   } catch (error) {
-    console.log('connect felid', error)
+    console.error('connect felid', error)
   }
 }
 main()
